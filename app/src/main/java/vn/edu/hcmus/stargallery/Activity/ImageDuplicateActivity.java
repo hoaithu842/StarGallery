7b1d8ad825160c7287f5e6a3bb16500b724e8e5e
hoaithu842 <hoaithu842@gmail.com>
2024-05-04 14:27:02 +0700

    Caching duplicate

--- app/src/main/java/vn/edu/hcmus/stargallery/Activity/ImageDuplicateActivity.java
+++ app/src/main/java/vn/edu/hcmus/stargallery/Activity/ImageDuplicateActivity.java
@@ -1,7 +1,10 @@
 package vn.edu.hcmus.stargallery.Activity;
 
 import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.SpannableStringBuilder;
 import android.text.style.StyleSpan;
@@ -9,14 +12,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.app.NotificationManager;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import vn.edu.hcmus.stargallery.Adapter.AlbumDetailAdapter;
 import vn.edu.hcmus.stargallery.Adapter.AlbumsViewAdapter;
@@ -24,6 +32,7 @@ import vn.edu.hcmus.stargallery.Helper.ImageDuplicateFinder;
 import vn.edu.hcmus.stargallery.R;
 
 public class ImageDuplicateActivity extends AppCompatActivity {
+    private static final String CACHE_KEY_DUPLICATED_IMAGES = "duplicated_images";
     String album_name;
     private static final int REQUEST_DELETE_ITEM = 100;
     ArrayList<String> images;
@@ -69,17 +78,17 @@ public class ImageDuplicateActivity extends AppCompatActivity {
             }
         });
 
-
-//        adapter.setOnClickListener(new AlbumDetailAdapter.OnClickListener() {
-//            @Override
-//            public void onClick(int position) {
-//                Intent intent = new Intent(ImagesDuplicatedActivity.this, ImageDetailActivity.class);
-//                intent.putExtra("image_path", images.get(position));
-//                intent.putStringArrayListExtra("images_list", images);
-//                startActivity(intent);
-//            }
-//        });
+        loadPrefernces();
         findDuplicate();
+
+        ImageButton syncButton = findViewById(R.id.sync_button);
+        syncButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Syncing", "Syncing");
+                loadPrefernces();
+            }
+        });
     }
 
     @SuppressLint("NotifyDataSetChanged")
@@ -87,19 +96,65 @@ public class ImageDuplicateActivity extends AppCompatActivity {
         ImageDuplicateFinder finder = new ImageDuplicateFinder(images, new ImageDuplicateFinder.OnDuplicateImageFoundListener() {
             @Override
             public void onDuplicateImageFound(List<List<String>> duplicateGroups) {
+                duplicated.clear();
                 for (List<String> group : duplicateGroups) {
                     if (group.size() > 1) {
                         duplicated.addAll(group);
                     }
                 }
+                saveDuplicatedImagesToCache();
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        adapter.notifyDataSetChanged();
+                        Toast.makeText(getApplicationContext(), "You can hit the sync button to reload newest fetch", Toast.LENGTH_SHORT).show();
+//                        adapter.notifyDataSetChanged();
                     }
                 });
             }
         });
         finder.findDuplicateImagesAsync();
+    }
+
+    private void saveDuplicatedImagesToCache() {
+        SharedPreferences preferences = getSharedPreferences("MyPreferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        Set<String> duplicatedSet = new HashSet<>(duplicated);
+        editor.putStringSet(CACHE_KEY_DUPLICATED_IMAGES, duplicatedSet);
+        editor.apply();
+        Log.d("Saved newest fetch", "Saved newest fetch");
+    }
+
+    private void loadPrefernces() {
+        SharedPreferences preferences = getSharedPreferences("MyPreferences", MODE_PRIVATE);
+        if (preferences.contains(CACHE_KEY_DUPLICATED_IMAGES)) {
+            duplicated.clear();
+            Set<String> duplicatedSet = preferences.getStringSet(CACHE_KEY_DUPLICATED_IMAGES, new HashSet<>());
+            duplicated.addAll(duplicatedSet);
+            adapter.notifyDataSetChanged();
+            if (duplicated.size()==0) {
+                Toast.makeText(this, "Fetched! The result is empty", Toast.LENGTH_SHORT).show();            }
+        } else {
+            Log.d("Empty", "Empty");
+            showNotification();
+        }
+    }
+
+    private void showNotification() {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            CharSequence name = "YourChannelName";
+            String description = "YourChannelDescription";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("my_channel_id", name, importance);
+            channel.setDescription(description);
+            notificationManager.createNotificationChannel(channel);
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "my_channel_id")
+                .setSmallIcon(R.drawable.icon_next)
+                .setContentTitle("First time access will take time analyzing")
+                .setContentText("Please hit the sync button again later to fetch the newest result.")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        notificationManager.notify(1, builder.build());
     }
 }
